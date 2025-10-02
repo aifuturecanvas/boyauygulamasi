b6d7df0834361ed82f068b5313c63b47362f93bb
aifuturecanvas <aifuturecanvas@gmail.com>
2025-10-02 12:56:49 +0300

    Delete utils directory

--- utils/base64.js
+++
@@ -1,8 +0,0 @@
-export function base64ToArrayBuffer(base64: string) {
-  const cleaned = base64.replace(/^data:image\/\w+;base64,/, '');
-  const binary = globalThis.atob ? atob(cleaned) : Buffer.from(cleaned, 'base64').toString('binary');
-  const len = binary.length;
-  const bytes = new Uint8Array(len);
-  for (let i = 0; i < len; i++) bytes[i] = binary.charCodeAt(i);
-  return bytes.buffer;
-}
